ba0cc434a64ea3c6499efceaaceacd7bdb1b5426
Kaspazza <kaspazza@gmail.com>
2020-03-02 23:04:37 +0100

    fixed bug with incorrect path output

--- src/main/scala/com/kaspazza/files/DirEntry.scala
+++ src/main/scala/com/kaspazza/files/DirEntry.scala
@@ -1,8 +1,13 @@
 package com.kaspazza.files
 
 abstract class DirEntry(val parentPath: String, val name: String) {
-  def path: String = parentPath + Directory.SEPARATOR + name
+  def path: String = {
+    val separatorIfNecessary =
+      if(Directory.ROOT_PATH.equals(parentPath)) ""
+      else Directory.SEPARATOR
 
+    parentPath + separatorIfNecessary + name
+  }
   def asDirectory: Directory
 
   def asFile: File
